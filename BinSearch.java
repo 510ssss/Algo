e430d63cdb92801cfdad8d1c11488c160f64db0a
SouUchida <14ec020@ms.dendai.ac.jp>
2015-10-08 09:31:05 +0900

    Update and rename binSearch.java to BinSearch.java

--- BinSearch.java
+++ BinSearch.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
-public class binSearch {
+public class BinSearch {
 
 	static int binSearch(int[] a, int n, int key){
 		int p1=0;
